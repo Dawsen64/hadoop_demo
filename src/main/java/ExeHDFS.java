e6630a882c5661be56baab30004595baacc70952
zqiusen <zqiusen@qq.com>
2022-03-20 23:08:01 +0800

    1

--- src/main/java/ExeHDFS.java
+++ src/main/java/ExeHDFS.java
@@ -1,8 +1,11 @@
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.Path;
 
 import java.io.IOException;
+//import java.net.URI;
+import java.net.URI;
 import java.net.URISyntaxException;
 
 /**
@@ -14,16 +17,16 @@ public class ExeHDFS {
     public void testView() throws IOException, URISyntaxException, InterruptedException {
         System.out.println("View file:");
         Configuration conf = new Configuration();
-        conf . set(" dfs. client . use . datanode . hostname", "true ");
-        conf . set("fs . defaultFS", "hdfs://121.36. 99.86:8020");
-        FileSystem ndfs = FileSystem. get(new URI( "hdfs://121.36. 99.86"), conf, "root");
+        conf.set("dfs.client.use.datanode.hostname", "true");
+        conf.set("fs.defaultFS", "hdfs://121.36.99.86:8020");
+        FileSystem hdfs = FileSystem.get(new URI("hdfs://121.36.99.86"), conf, "root");
         Path path = new Path(hdfsPath);
-        FiLeStatus[] list = hdfs.listStatus(path);
+        FileStatus[] list = hdfs.listStatus(path);
         if(list.length==0){
-            System. out . printLn("HDFS is empty.");
+            System.out.println("HDFS is empty.");
         }else {
-            for (FiLeStatus f : list) {
-                System. out . printf("name: %s， folder: %s，size: %d\n"， f. getPath(), f. isDirectory(), f.getLen());
+            for (FileStatus f : list) {
+                System.out.printf("name: %s， folder: %s，size: %d\n"， f. getPath(), f. isDirectory(), f.getLen());
             }
 
         }
